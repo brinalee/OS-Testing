8db10afae6b32e57a934deaf905458156701f433
Adrian Guthals <aguthals@aguthals-desktop.(none)>
2011-11-02 19:50:28 -0700

    * modified

--- benchmarks.c
+++ benchmarks.c
@@ -644,7 +644,7 @@ long long getPageFaultOverhead(void)
 	int idx1, ref1;
 	long long time1, time2, locOverhead, totalOverhead, numPageFaults;
 	
-	long long numPageAccesses = 100000;
+	long long numPageAccesses = 1000000;
 	long long minPageOv = 100000;
 	
 	int stride = 9973;
