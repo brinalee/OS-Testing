cc4d2ffdc6979d4134571233f6ca3e65939b816d
Adrian Guthals <aguthals@aguthals-desktop.(none)>
2011-12-06 16:53:12 -0800

    *modified

--- randRemoteFileRead.c
+++ randRemoteFileRead.c
@@ -16,9 +16,9 @@ int main(int argc, char *argv[])
 	printf("<log file size>\t<log cycle latency per block>\n");
 	for (int i = 12; i <= 31; i += 1)
 	{
-		sprintf(filename, "remote_mount/test_rand_log%d.dat", i);
+		sprintf(filename, "remote_mount/test_rand_log.dat");
 		//printf("%s\n", filename);
-		double timeR = getRandomFileReadTime(i, filename, false);
+		double timeR = getRandomFileReadTime(i, filename, true);
 		printf("%d\t%.2lf\n", i, timeR);
 		fflush(stdout);
 	}
--- seqRemoteFileRead.c
+++ seqRemoteFileRead.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
 	printf("<log file size>\t<log cycle latency per block>\n");
 	for (int i = 12; i <= 31; i += 1)
 	{
-		sprintf(filename, "remote_mount/test_seq_log%d.dat", i);
-		double timeR = getSequentialFileReadTime(i, filename, false);
+		sprintf(filename, "remote_mount/test_seq_log.dat");
+		double timeR = getSequentialFileReadTime(i, filename, true);
 		printf("%d\t%.2lf\n", i, timeR);
 		fflush(stdout);
 	}
